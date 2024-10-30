b5797e2d135f119042c98cbf0aa50da92169be06
Jakob Duerrwaechter <jakob.duerrwaechter@pik-potsdam.de>
2024-10-30 11:00:35 +0100

    remove mrremind from dependencies

--- R/madrat.R
+++ R/madrat.R
@@ -1,20 +1,20 @@
-# .onLoad <- function(libname, pkgname) {
-#   madrat::madratAttach(c(pkgname, "mrremind", "GDPuc", "mrcommons", "mrdrivers"))
-# }
-# 
-# 
-# .onUnload <- function(libpath) {
-#   madrat::madratDetach(c(libpath, "mrremind", "GDPuc", "mrcommons", "mrdrivers"))
-# }
-
-
-.onAttach <- function(libname, pkgname) {
-  madrat::madratAttach(pkgname)
+.onLoad <- function(libname, pkgname) {
+  madrat::madratAttach(c(pkgname, "mrremind", "GDPuc", "mrcommons", "mrdrivers"))
 }
 
-.onDetach <- function(libpath) {
-  madrat::madratDetach(libpath)
+
+.onUnload <- function(libpath) {
+  madrat::madratDetach(c(libpath, "mrremind", "GDPuc", "mrcommons", "mrdrivers"))
 }
+
+
+# .onAttach <- function(libname, pkgname) {
+#   madrat::madratAttach(pkgname)
+# }
+#
+# .onDetach <- function(libpath) {
+#   madrat::madratDetach(libpath)
+# }
 
 
 # redirect standard messaging functions to vcat
