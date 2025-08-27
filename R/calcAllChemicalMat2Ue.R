6f91b139469f8d91c52272d79e9012f0a148dba3
Leonie Schweiger <leonie.schweiger@pik-potsdam.de>
2025-08-27 17:10:29 +0200

    extend mat2ue projections to 2150

--- R/calcAllChemicalMat2Ue.R
+++ R/calcAllChemicalMat2Ue.R
@@ -27,7 +27,6 @@ calcAllChemicalMat2Ue <- function() {
     as.data.frame() %>%
     select(-Cell) %>%
     mutate(Year = as.numeric(as.character(Year))) %>%   # Convert factor to character then numeric
-    filter(Year %in% c(2020,2025,2030,2035,2040,2045,2050)) %>%
     group_by(Region) %>%
     mutate(Ratio = Value / Value[Year == 2020]) %>%
     ungroup()
@@ -104,7 +103,6 @@ calcAllChemicalMat2Ue <- function() {
     select(-Cell)%>%
     filter(!Region %in% "GLO")%>%
     mutate(Year = as.numeric(as.character(Year))) %>%
-    filter(Year %in% c(2020,2025,2030,2035,2040,2045,2050))%>%
     group_by(Region) %>%
     mutate(Ratio = Value / Value[Year == 2020]) %>%
     ungroup()
@@ -124,13 +122,17 @@ calcAllChemicalMat2Ue <- function() {
     dplyr::left_join(p37_mat2ue, by = c("Data1" = "Product")) %>%
     dplyr::mutate(new_mat2ue = mat2ue / fe_change)
   
-  # Extend the data: For each Region and Data1 group, ensure rows exist for 2050, 2055, ..., 2100.
-  final_data <- merged_data %>%
+  # Extend the data: For each Region and Data1 group, ensure rows exist for 2050, 2055, ..., 2150. (assume increase of ammonia and methanol final demand is the same as of ue_chemicals)
+  years <- merged_data %>% select(Year) %>% distinct() %>% filter(Year>2050)
+  extended_years <- merged_data %>% filter(Year==2050, Data1 %in% c("ammoFinal","methFinal")) %>%
+    select(-Year) %>% crossing(years)
+  final_data <- merged_data %>% 
+    rbind(extended_years) %>%
+    filter(Year >= 2020) %>%
     mutate(all_in = "ue_chemicals"
     ) %>%
     select(Region,Year,Data1,all_in,new_mat2ue)
   
-
   x <- as.magpie(final_data, spatial = 1, temporal = 2)
   map <- toolGetMapping("regionmappingH12.csv", type = "regional", where = "mrindustry")
   x <- toolAggregate(x, rel = map, dim = 1, from = "RegionCode", to = "CountryCode")
@@ -147,7 +149,7 @@ calcAllChemicalMat2Ue <- function() {
     x = x,
     weight = weight,
     unit = "2017$/kg or 2017$/kgN",  # Specify units based on conversion factors
-    description = "Calculates the material-to-UE conversion factors for 2020-2050 on country level."
+    description = "Calculates the material-to-UE conversion factors for 2020-2150 on country level."
   ))
 }
 
