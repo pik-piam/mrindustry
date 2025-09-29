eaa1bd9d10d6afaec06e2e4b1a4e807e6283398e
Bennet Weiss <bennet.weiss@pik-potsdam.de>
2025-09-29 19:36:25 +0200

    apply function naming scheme and move dimensions to note

--- R/fullMFA.R
+++ R/fullMFA.R
@@ -51,17 +51,16 @@ fullMFA <- function(rev = 0, dev = "", scenario='SSP2', gdp_per_capita=FALSE) {
   #  ------------- CEMENT -----------
   
     # Production
-
-  calcOutput("BinderProduction", file = "cement_production.cs4r", years=1900:2023, subtype="cement")
+  calcOutput("cBinderProduction", file = "cement_production.cs4r", years=1900:2023, subtype="cement")
 
     # Trade
   
-  calcOutput("MaterialTrade", file = "cement_trade.cs4r", years=1900:2023, subtype="cement")
-  calcOutput("MaterialTrade", file = "clinker_trade.cs4r", years=1900:2023, subtype="clinker")
+  calcOutput("cMaterialTrade", file = "cement_trade.cs4r", years=1900:2023, subtype="cement")
+  calcOutput("cMaterialTrade", file = "clinker_trade.cs4r", years=1900:2023, subtype="clinker")
 
     # Parameters
 
-  calcOutput("BuiltLifespan", file = "use_lifetime_mean.cs4r")
-  calcOutput("ClinkerRatio", file = "clinker_ratio.cs4r", years=1900:2023)
+  calcOutput("aBuiltLifespan", file = "use_lifetime_mean.cs4r")
+  calcOutput("cClinkerRatio", file = "clinker_ratio.cs4r", years=1900:2023)
 
 }
