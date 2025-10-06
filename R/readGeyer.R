4d6420498fb96b73d0d89f34c8fd1d325edccb35
Leonie Schweiger <leonie.schweiger@pik-potsdam.de>
2025-10-06 13:47:46 +0200

    read and calc functions for plastic lifetimes

--- R/readGeyer.R
+++ R/readGeyer.R
@@ -1,8 +1,9 @@
 #' Read Geyer et al. SI Global plastic production 1950-2015
 #'
-#' @param subtype Character string specifying the dataset and scope.
-#'        For now only one subtype, but SI contains more data that we might to read in in the future:
+#' @param subtype Character string specifying the dataset and scope
 #'        - "Prod_1950-2015"
+#'        - "Lifetime_mean"
+#'        - "Lifetime_std"
 #'
 #' @return magpie object of the Geyer et al. Plastic data
 #'
@@ -16,7 +17,7 @@
 #' @importFrom readxl read_excel
 #' @importFrom dplyr select filter
 #' @importFrom magclass as.magpie getComment<-
-#' 
+#'
 readGeyer <- function(subtype) {
   # ---------------------------------------------------------------------------
   # Map key to Excel file parameters
@@ -26,34 +27,42 @@ readGeyer <- function(subtype) {
                      sheet  = "Table S1",
                      range  = "A1:B67"
                    ),
+                   "Lifetime_mean" = list(
+                     file   = "Geyer.xlsx",
+                     sheet  = "Table S4",
+                     range  = "A1:C9"
+                   ),
+                   "Lifetime_std" = list(
+                     file   = "Geyer.xlsx",
+                     sheet  = "Table S4",
+                     range  = "A1:C9"
+                   ),
                    stop("Invalid subtype: ", subtype)
   )
-  
+
   # ---------------------------------------------------------------------------
   # Read raw data from Excel
-  raw_df <- read_excel(
-    path  = "C:/Users/leoniesc/madrat/sources/Geyer/Geyer.xlsx", #params$file,
+  df <- read_excel(
+    path  = params$file,
     sheet = params$sheet,
     range = params$range,
     skip  = 1
   )
-  
-  # ---------------------------------------------------------------------------
-  # Select and filter columns based on subtype
-  df <- switch(
-    subtype,
-    # Plastic use or waste by region
-    "Prod_1950-2015" = raw_df %>%
-      select(`Year`, `Global Resin Production (Mt)`),
-    stop("Unsupported subtype: ", subtype)
-  )
-  
+
   # ---------------------------------------------------------------------------
   # Convert to magpie object
   # ---------------------------------------------------------------------------
-  magpie_data <- as.magpie(df, temporal = 1)
+  if(subtype=="Prod_1950-2015"){
+    magpie_data <- as.magpie(df, temporal = 1)
+  }
+  if(subtype=="Lifetime_mean"){
+    magpie_data <- as.magpie(df %>% select(`Market Sector`, `Mean (in years)`))
+  }
+  if(subtype=="Lifetime_std"){
+    magpie_data <- as.magpie(df %>% select(`Market Sector`, `Standard deviation`))
+  }
   getComment(magpie_data) <- subtype
-  
+
   return(magpie_data)
 }
 
