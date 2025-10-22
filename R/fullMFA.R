2603bb4e83efca15f4a4dc266b5aae45bf327075
Bennet Weiss <bennet.weiss@pik-potsdam.de>
2025-10-22 11:31:32 +0200

    add material prefixes

--- R/fullMFA.R
+++ R/fullMFA.R
@@ -38,39 +38,39 @@ fullMFA <- function(rev = 0, dev = "", scenario='SSP2', gdp_per_capita=FALSE, ru
 
   #  ------------- DRIVERS -------------
   if (runSection("drivers")) {
-    calcOutput("Population1900To2150", file = "population1900To2150.cs4r", scenario=scenario)
-    calcOutput("GDP1900To2150", file = "gdp1900To2150.cs4r", scenario=scenario, per_capita=gdp_per_capita)
+    calcOutput("Population1900To2150", file = "co_population1900To2150.cs4r", scenario=scenario)
+    calcOutput("GDP1900To2150", file = "co_gdp1900To2150.cs4r", scenario=scenario, per_capita=gdp_per_capita)
   }
 
   #  ------------- STEEL ----------------
   if (runSection("steel")) {
     # Production
-    calcOutput("SteelProduction", file = "steel_production.cs3r")
+    calcOutput("SteelProduction", file = "st_steel_production.cs4r")
     # Trade
-    calcOutput("SteelTrade", file = "steel_imports.cs3r", subtype='imports')
-    calcOutput("SteelTrade", file = "steel_exports.cs3r", subtype='exports')
-    calcOutput("SteelTrade", file = "steel_scrap_imports.cs3r", subtype='scrap_imports')
-    calcOutput("SteelTrade", file = "steel_scrap_exports.cs3r", subtype='scrap_exports')
-    calcOutput("SteelTrade", file = "steel_indirect_imports.cs3r", subtype='indirect_imports')
-    calcOutput("SteelTrade", file = "steel_indirect_exports.cs3r", subtype='indirect_exports')
+    calcOutput("SteelTrade", file = "st_steel_imports.cs4r", subtype='imports')
+    calcOutput("SteelTrade", file = "st_steel_exports.cs4r", subtype='exports')
+    calcOutput("SteelTrade", file = "st_steel_scrap_imports.cs4r", subtype='scrap_imports')
+    calcOutput("SteelTrade", file = "st_steel_scrap_exports.cs4r", subtype='scrap_exports')
+    calcOutput("SteelTrade", file = "st_steel_indirect_imports.cs4r", subtype='indirect_imports')
+    calcOutput("SteelTrade", file = "st_steel_indirect_exports.cs4r", subtype='indirect_exports')
     # Parameters
-    calcOutput("SteelStaticParameters", file = "steel_static_parameters.cs3r")
-    calcOutput("CullenFabricationYield", file = "fabrication_yield.cs3r", aggregate=FALSE)
-    calcOutput("SteelLifetimes", subtype='Cooper2014', file = "lifetimes.cs3r", aggregate=FALSE)
-    calcOutput("SteelRecoveryRate", subtype='WorldSteel', file = "recovery_rate.cs3r", aggregate=FALSE)
-    calcOutput("SteelSectorSplits", subtype='Pauliuk2013', file = "sector_splits.cs3r", aggregate=FALSE)
+    calcOutput("SteelStaticParameters", file = "st_steel_static_parameters.cs4r")
+    calcOutput("CullenFabricationYield", file = "st_fabrication_yield.cs4r", aggregate=FALSE)
+    calcOutput("SteelLifetimes", subtype='Cooper2014', file = "st_lifetimes.cs4r", aggregate=FALSE)
+    calcOutput("SteelRecoveryRate", subtype='WorldSteel', file = "st_recovery_rate.cs4r", aggregate=FALSE)
+    calcOutput("SteelSectorSplits", subtype='Pauliuk2013', file = "st_sector_splits.cs4r", aggregate=FALSE)
   }
 
   #  ------------- CEMENT -----------
   if (runSection("cement")) {
     # Production
-    calcOutput("MCeBinderProduction", file = "cement_production.cs4r", years=1900:2023, subtype="cement")
+    calcOutput("MCeBinderProduction", file = "ce_cement_production.cs4r", years=1900:2023, subtype="cement")
     # Trade
-    calcOutput("MCeMaterialTrade", file = "cement_trade.cs4r", years=1900:2023, subtype="cement")
-    calcOutput("MCeMaterialTrade", file = "clinker_trade.cs4r", years=1900:2023, subtype="clinker")
+    calcOutput("MCeMaterialTrade", file = "ce_cement_trade.cs4r", years=1900:2023, subtype="cement")
+    calcOutput("MCeMaterialTrade", file = "ce_clinker_trade.cs4r", years=1900:2023, subtype="clinker")
     # Parameters
-    calcOutput("MCeBuiltLifespan", file = "use_lifetime_mean.cs4r")
-    calcOutput("MCeClinkerRatio", file = "clinker_ratio.cs4r", years=1900:2023)
+    calcOutput("MCeBuiltLifespan", file = "ce_use_lifetime_mean.cs4r")
+    calcOutput("MCeClinkerRatio", file = "ce_clinker_ratio.cs4r", years=1900:2023)
   }
 
 }
