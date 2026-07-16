c1118f12f092e0c5d9d887e06fbf8af1f775fa3d
Leonie Schweiger <leonie.schweiger@pik-potsdam.de>
2026-07-16 17:53:28 +0200

    update Stegmann read and convert function to allow disaggregation by population or FE

--- R/readStegmann2022.R
+++ R/readStegmann2022.R
@@ -8,10 +8,13 @@
 #' Link to SI:
 #' https://static-content.springer.com/esm/art%3A10.1038%2Fs41586-022-05422-5/MediaObjects/41586_2022_5422_MOESM1_ESM.xlsx #nolint
 #'
+#' @param subtype Character. "FEweighted" disaggregates all variables based on
+#' FE; "PopWeighted" disaggregates all variables based on Population 
+#' (case distinction in convertStegmann2022)
 #' @return a magpie object of the data
-#' @author Falk Benke, Simón Moreno
+#' @author Falk Benke, Simón Moreno, Leonie Schweiger
 #'
-readStegmann2022 <- function() {
+readStegmann2022 <- function(subtype) {
   readxl::read_xlsx("41586_2022_5422_MOESM1_ESM.xlsx", sheet = "Data") %>%
     tidyr::pivot_longer(tidyselect::starts_with("2"), names_to = "period") %>%
     as.magpie(spatial = "Region", temporal = "period", tidy = TRUE)
